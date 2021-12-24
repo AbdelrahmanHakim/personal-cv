a1458be3aab4daa3389aa68c1013f1c6671ed20a
AbdelrhmanHakim <abdelrahman.abdelhakim@outlook.com>
2021-12-24 15:22:24 +0200

    navbar and menulist

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>        
-    )
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I am a full-stack developer based in Egypt!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Abdelrahman Abdelhakim
+          </Heading>
+          <p>Innovative Engineer (Developer / Athelete)</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 export default Page
